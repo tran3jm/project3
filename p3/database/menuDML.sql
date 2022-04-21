a2e6d38c0aa408096049becb95bb4903bd75e428
Seth Walter <waltersb@dukes.jmu.edu>
2022-04-21 16:35:03 -0400

    insert food items into db

---
+++ p3/database/menuDML.sql
@@ -0,0 +1,26 @@
+/* inserts all menu items into the modernlilydb -> foods table */
+USE modernlilydb;
+
+/* drinks */
+INSERT INTO Foods VALUES (1, 'Pepsi', 2.50);
+INSERT INTO Foods VALUES (2, 'Diet Pepsi', 2.50);
+INSERT INTO Foods VALUES (3, 'Sprite', 2.50);
+INSERT INTO Foods VALUES (4, 'Ramune', 3.00);
+INSERT INTO Foods VALUES (5, 'Calpico', 3.00);
+INSERT INTO Foods VALUES (6, 'Iced Tea', 2.50);
+INSERT INTO Foods VALUES (7, 'Hot Tea', 3.50);
+INSERT INTO Foods VALUES (8, 'Thai Tea', 3.75);
+INSERT INTO Foods VALUES (9, 'Vietnamese Coffee', 4.50);
+INSERT INTO Foods VALUES (10, 'Boba', 4.50);
+
+/* food items */
+INSERT INTO Foods VALUES (11, 'Drunken Noodles', 12.50);
+INSERT INTO Foods VALUES (12, 'Dumplings', 8.50);
+INSERT INTO Foods VALUES (13, 'Crispy Pork Belly', 7.50);
+INSERT INTO Foods VALUES (14, 'Pho', 10.50);
+INSERT INTO Foods VALUES (15, 'Banh Mi', 9.35);
+INSERT INTO Foods VALUES (16, 'Fusion BiBamBop', 11.50);
+INSERT INTO Foods VALUES (17, 'Pork Buns', 8.50);
+INSERT INTO Foods VALUES (18, 'Fried Eggplant', 6.75);
+INSERT INTO Foods VALUES (19, 'Sour Cabbage Soup', 11.50);
+INSERT INTO Foods VALUES (20, 'Lotus Soup', 9.50);
